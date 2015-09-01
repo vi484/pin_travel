b6cf6a166ac56f9293eca7385a2fda399171856b
vi_484 <virginianguyen484@gmail.com>
2015-09-01 08:52:36 +0000

    Add users avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :pins
   has_many :comments
+  
+  has_attached_file :avatar, styles: { medium: "100x100>", :thumb => "20x20#" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831183421) do
+ActiveRecord::Schema.define(version: 20150901075939) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -52,6 +52,10 @@ ActiveRecord::Schema.define(version: 20150831183421) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
